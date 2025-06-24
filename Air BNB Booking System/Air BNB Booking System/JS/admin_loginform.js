2d7da257f87879973aca4c7e6016d913e42afb20
lornatividad15 <lornatividad15@gmail.com>
2025-06-25 00:08:42 +0800

    Delete Air BNB Booking System/Air BNB Booking System directory

--- Air BNB Booking System/Air BNB Booking System/JS/admin_loginform.js
+++
@@ -1,8 +0,0 @@
-function closeModal() {
-  const modalOverlay = document.getElementById('modalOverlay');
-  if (modalOverlay) modalOverlay.classList.remove('show');
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-  setTimeout(closeModal, 3000);
-});
